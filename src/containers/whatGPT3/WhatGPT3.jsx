b6eeca3c58d7f979be5879ee050f29ab444c0547
VJS <minsung_96@naver.com>
2021-12-14 23:56:18 +0900

    Feature Introduction CSS #1

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components/feature/Feature'
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            
+        <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+            <div className="gpt3__whatgpt3-feature">
+                <Feature />
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">
+                    Well, just it existed in Mte and Uringeshun
+                </h1>
+                <p>
+                    Explore other Library
+                </p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
         </div>
     )
 }
