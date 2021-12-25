27cf11e7192abfae752f11ab16da4005feec2646
VJS <minsung_96@naver.com>
2021-12-25 23:59:41 +0900

    Article @1

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './blog.css'
 import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 
 const Blog = () => {
     return (
@@ -12,10 +13,13 @@ const Blog = () => {
             </div>
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupA">
-
+                    <Article imgUrl={blog01} date="Oct 12, 2015" title="Beginning of the Legend"/>
                 </div>
                 <div className="gpt3__blog-container_groupB">
-
+                    <Article imgUrl={blog02} date="Dec 18, 2020" title="Moving Man"/> 
+                    <Article imgUrl={blog03} date="Dex 29, 2020" title="MoongTangYee World"/>
+                    <Article imgUrl={blog04} date="Oct 2, 2015" title="Just One More Match"/>
+                    <Article imgUrl={blog05} date="Oct 31, 2018" title="I!!! DID!!! FIREBALL!!!"/>
                 </div>
             </div>
         </div>
